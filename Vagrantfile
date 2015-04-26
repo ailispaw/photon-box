28c54f293eadd36bf141a27dc8f8229555601d28
A.I <ailis@paw.zone>
2015-04-26 09:49:01 -0700

    Fix configure_networks
    - Support type: "dhcp"

--- Vagrantfile
+++ Vagrantfile
@@ -5,7 +5,10 @@ Vagrant.configure(2) do |config|
 
   config.vm.hostname = "photon-test.example.com"
 
-  config.vm.network "private_network", ip: "192.168.33.10"
+  config.vm.network :private_network, ip: "192.168.33.10"
+#  config.vm.network :private_network, type: "dhcp"
+#  config.vm.network :public_network, bridge: "en0: Wi-Fi (AirPort)"
+#  config.vm.network :public_network, bridge: "en0: Wi-Fi (AirPort)", ip: "192.168.1.201"
 
   config.vm.synced_folder ".", "/vagrant", type: "nfs", mount_options: ["nolock", "vers=3", "udp"]
 
--- vagrant_plugin_guest_photon.rb
+++ vagrant_plugin_guest_photon.rb
@@ -6,6 +6,32 @@ unless Vagrant.has_plugin?("vagrant-guests-photon")
   exit
 end
 
+require 'tempfile'
+require 'ipaddr'
+
+# Borrowed from http://stackoverflow.com/questions/1825928/netmask-to-cidr-in-ruby
+IPAddr.class_eval do
+  def to_cidr
+    self.to_i.to_s(2).count("1")
+  end
+end
+
+STATIC_NETWORK = <<EOF
+[Match]
+Name=%s
+
+[Network]
+Address=%s
+EOF
+
+DHCP_NETWORK = <<EOF
+[Match]
+Name=%s
+
+[Network]
+DHCP=yes
+EOF
+
 require Vagrant.user_data_path.join("gems/gems/vagrant-guests-photon-1.0.0/lib/vagrant-guests-photon/cap/configure_networks.rb")
 module VagrantPlugins
   module GuestPhoton
@@ -15,16 +41,32 @@ module VagrantPlugins
 
         def self.configure_networks(machine, networks)
           machine.communicate.tap do |comm|
+            comm.sudo("rm -f /etc/systemd/network/50-vagrant-*.network")
+
             networks.each do |network|
               iface = "eth#{network[:interface]}"
+              unit_name = "50-vagrant-%s.network" % [iface]
 
               if network[:type] == :static
-                comm.sudo("ifconfig #{iface} #{network[:ip]} netmask #{network[:netmask]}")
+                cidr = IPAddr.new(network[:netmask]).to_cidr
+                address = "%s/%s" % [network[:ip], cidr]
+                unit_file = STATIC_NETWORK % [iface, address]
+              elsif network[:type] == :dhcp
+                unit_file = DHCP_NETWORK % [iface]
               end
 
-              comm.sudo("ip link set #{iface} down")
-              comm.sudo("ip link set #{iface} up")
+              temp = Tempfile.new("vagrant")
+              temp.binmode
+              temp.write(unit_file)
+              temp.close
+
+              comm.upload(temp.path, "/tmp/#{unit_name}")
+              comm.sudo("mv /tmp/#{unit_name} /etc/systemd/network/")
+              comm.sudo("chown root:root /etc/systemd/network/#{unit_name}")
+              comm.sudo("chmod +r /etc/systemd/network/#{unit_name}")
             end
+
+            comm.sudo("systemctl restart systemd-networkd.service")
           end
         end
       end
