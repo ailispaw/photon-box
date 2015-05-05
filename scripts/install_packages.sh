223db793e4f805d7e0dc5109e764dec1fcec9633
A.I <ailis@paw.zone>
2015-05-05 11:24:31 -0700

    Bump rpcbind to v0.2.3

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -35,10 +35,10 @@ rm -rf /tmp/libtirpc-0.2.5
 # http://www.linuxfromscratch.org/blfs/view/svn/basicnet/rpcbind.html
 # http://sourceforge.net/projects/rpcbind/files/rpcbind/
 cd /tmp
-curl -OL http://downloads.sourceforge.net/rpcbind/rpcbind-0.2.2.tar.bz2
-bzip2 -d rpcbind-0.2.2.tar.bz2
-tar xf rpcbind-0.2.2.tar
-cd rpcbind-0.2.2
+curl -OL http://downloads.sourceforge.net/rpcbind/rpcbind-0.2.3.tar.bz2
+bzip2 -d rpcbind-0.2.3.tar.bz2
+tar xf rpcbind-0.2.3.tar
+cd rpcbind-0.2.3
 sed -i "/servname/s:rpcbind:sunrpc:" src/rpcbind.c &&
 sed -i "/error = getaddrinfo/s:rpcbind:sunrpc:" src/rpcinfo.c
 
