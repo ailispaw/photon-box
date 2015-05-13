d6037c11684dc72c2481a4e41d310e62dcacde7b
A.I <ailis@paw.zone>
2015-05-13 15:32:44 -0700

    Bump VirtualBox Guest Additions to v4.3.28

--- scripts/install_vboxguest.sh
+++ scripts/install_vboxguest.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 set -e
 
-cd /usr/src
-curl -OL https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.19.2.tar.xz
-tar xPJf linux-3.19.2.tar.xz
+KERNEL_VERSION=3.19.2
+VBOX_VERSION=4.3.28
 
-cd linux-3.19.2
+cd /usr/src
+curl -OL https://www.kernel.org/pub/linux/kernel/v3.x/linux-${KERNEL_VERSION}.tar.xz
+tar xPJf linux-${KERNEL_VERSION}.tar.xz
+
+cd linux-${KERNEL_VERSION}
 curl -L https://raw.githubusercontent.com/vmware/photon/master/support/package-builder/config -o .config
 make oldconfig
 make prepare && make scripts
@@ -20,17 +23,17 @@ cd /media/GuestAdditionsISO
 
 cd /vboxguest
 mkdir -p amd64 && tar -C amd64 -xjf VBoxGuestAdditions-amd64.tar.bz2
-rm -rf amd64/src/vboxguest-4.3.26/vboxvideo
-KERN_DIR=/usr/src/linux-3.19.2 KERN_INCL=/usr/src/include make -C amd64/src/vboxguest-4.3.26
+rm -rf amd64/src/vboxguest-${VBOX_VERSION}/vboxvideo
+KERN_DIR=/usr/src/linux-${KERNEL_VERSION} KERN_INCL=/usr/src/include make -C amd64/src/vboxguest-${VBOX_VERSION}
 
-cp amd64/src/vboxguest-4.3.26/*.ko /lib/modules/3.19.2/
+cp amd64/src/vboxguest-${VBOX_VERSION}/*.ko /lib/modules/${KERNEL_VERSION}/
 cp amd64/lib/VBoxGuestAdditions/mount.vboxsf /sbin
 depmod -a
 
 umount /media/GuestAdditionsISO
 
-rm -f /usr/src/linux-3.19.2.tar.xz
-rm -rf /usr/src/linux-3.19.2
+rm -f /usr/src/linux-${KERNEL_VERSION}.tar.xz
+rm -rf /usr/src/linux-${KERNEL_VERSION}
 rm -rf /usr/src/include
 rm -rf /vboxguest
 rm -f /root/VBoxGuestAdditions.iso
